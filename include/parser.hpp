0ba78227788644221d1fe475474cc7972e44347e
azhz1107cat <105213176+azhz1107cat@users.noreply.github.com>
2025-10-25 23:39:44 +0800

    更新 parser.hpp

--- include/parser.hpp
+++ include/parser.hpp
@@ -5,4 +5,40 @@
  * @date 2025-10-25
  */
 
-#pragma once  
+#pragma once
+
+namespace kiz {
+
+class Parser {
+    const std::vector<Token> tokens_;
+    long long unsigned int curr_tok_idx_ = 0;
+public:
+    explicit Parser(const std::vector<Token>& tokens);
+    ~Parser() = default;
+
+    Token skip_token(const std::string& want_skip = "");
+    void skip_end_of_ln();
+    void skip_start_of_block();
+    [[nodiscard]] Token curr_token() const;
+
+    std::vector<std::unique_ptr<Statement>> parse();
+
+    // parse stmt
+    std::unique_ptr<Statement> parse_stmt();
+    std::unique_ptr<BlockStmt> parse_block();
+
+    // parse expr
+    std::unique_ptr<Expression> parse_expr();
+    std::unique_ptr<Expression> parse_comparison();
+    std::unique_ptr<Expression> parse_add_sub();
+    std::unique_ptr<Expression> parse_mul_div_mod();
+    std::unique_ptr<Expression> parse_power();
+    std::unique_ptr<Expression> parse_unary();
+    std::unique_ptr<Expression> parse_factor();
+
+    // parse factor
+    std::unique_ptr<Expression> parse_primary();
+    std::vector<std::unique_ptr<Expression>> parse_params(LexerTokenType endswith);
+};
+
+} // namespace kiz
