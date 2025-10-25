be56399df2106f5efea9d6c99528922b1fe45504
azhz1107cat <105213176+azhz1107cat@users.noreply.github.com>
2025-10-25 17:03:23 +0800

    创建 repl.cpp

---
+++ src/ui/repl.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file repl.cpp
+ * @brief 交互式终端（Read Evaluate Print Loop，简称 REPL）核心实现
+ * 
+ * @author azhz1107cat
+ * @date 2025-10-25
+ */
+
+namespace ui{
+
+Repl::Repl() {
+    // ToDo: ...
+}
+
+std::string Repl::read() {
+    // ToDo: ...
+}
+
+void Repl::loop() {
+    // ToDo: ...
+}
+
+void Repl::eval_and_print() {
+    // ToDo: ...
+}
+
+void Repl::~Repl() {
+    // ToDo: ...
+}
+
+} // namespace ui
