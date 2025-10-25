7492bf76f9ac38c237d5b3aeb9ca6a1b9188cfe1
azhz1107cat <105213176+azhz1107cat@users.noreply.github.com>
2025-10-25 18:30:44 +0800

    创建 opcode.hpp

---
+++ include/opcode.hpp
@@ -0,0 +1,25 @@
+/**
+ * @file opcode.hpp
+ * @brief 虚拟机指令集(VM Opcode)核心定义
+ * 执行IR
+ * @author azhz1107cat
+ * @date 2025-10-25
+ */
+
+namespace kiz {
+
+enum class Opcode {
+    OP_ADD, OP_SUB, OP_MUL, OP_DIV
+    OP_MOD, OP_POW, OP_NEG
+    OP_EQ, OP_GT, OP_LT
+    OP_AND, OP_NOT, OP_OR,
+    OP_IS, OP_IN,
+    CALL, RET,
+    GET_ATTR, SET_ATTR,
+    LOAD_VAR, LOAD_CONST,
+    SET_GLOBAL, SET_LOCAL, SET_NONLOCAL,
+    JUMP, JUMP_IF_FALSE
+
+};
+
+} // namespace kiz
