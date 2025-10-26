57f7a7241681ff0264b8a6021c323a83be627243
azhz1107cat <105213176+azhz1107cat@users.noreply.github.com>
2025-10-26 12:14:15 +0800

    更新 vm.cpp

--- src/vm.cpp
+++ src/vm.cpp
@@ -5,3 +5,23 @@
  * @author azhz1107cat
  * @date 2025-10-25
  */
+
+void kiz::Vm::load(model::Module* src_module) {
+
+}
+
+VmState kiz::Vm::exec(std::vector<Introduction> introduction) {
+
+}
+
+void kiz::Vm::eval_call() {
+
+}
+
+void kiz::Vm::eval_set_member() {
+
+}
+
+void kiz::Vm::eval_ret() {
+
+}
