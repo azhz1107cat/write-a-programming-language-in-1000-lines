91ee580220a5a3f03918a6cb6f0b5fad4885dd51
azhz1107cat <105213176+azhz1107cat@users.noreply.github.com>
2025-10-26 08:54:34 +0800

    更新 ir_gen.hpp

--- include/ir_gen.hpp
+++ include/ir_gen.hpp
@@ -5,3 +5,37 @@
  * @author azhz1107cat
  * @date 2025-10-25
  */
+
+#pragma once  
+
+namespace kiz {
+
+class IRGenerator {
+    std::unique_ptr<AstNode> ast;
+    std::stack<std::string> block_stack;
+
+    std::vector<std::string> curr_names;
+    std::vector<Instruction> curr_code_list;
+    std::vector<model::Object*> curr_const;
+    std::vector<std::tuple<size_t, size_t>> curr_lineno_map;
+
+public:
+    model::Module* gen();
+    void gen_mod();
+    void gen_expr();
+    void gen_fn_call();
+    void gen_dict()
+    void gen_fn_decl();
+    void gen_if();
+    void gen_while();
+    void gen_literal();
+
+protect:
+    model::CodeObject* make_code_obj();
+    model::Int* make_int_obj();
+    model::Dec* make_dec_obj();
+    model::Rational* make_rational_obj();
+    model::String* make_string_obj();
+};
+
+} // namespace kiz
