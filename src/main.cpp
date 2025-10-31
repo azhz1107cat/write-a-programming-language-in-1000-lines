01eeeffda386960288f3aa9c75092cb7e00fd442
azhz1107cat <105213176+azhz1107cat@users.noreply.github.com>
2025-11-01 00:21:10 +0800

    更新 main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,11 @@
  */
 
 /**
+ * 提供命令行帮助信息函数
+ */
+void show_help(const char* prog_name);
+
+/**
  * 命令行参数解析函数
  */
 void args_parser(int argc, char* argv[]);
@@ -21,12 +26,68 @@ int main(int argc, char* argv[]) {
 }
 
 /**
+ * 提供命令行帮助信息函数
+ */
+void show_help(const char* prog_name) {
+    printf("%s [指令] [参数]\n", prog_name);
+    printf("指令:\n");
+    printf("  没有参数      启动 REPL\n");
+    printf("  repl         启动 REPL\n");
+    printf("  <路径>        执行位于指定路径的kiz文件\n");
+    printf("  run <路径>    执行位于指定路径的kiz文件\n");
+    printf("  version      展示版本号\n");
+    printf("  help         展示帮助\n");
+}
+
+/**
  * @brief 解析命令行参数（argc/argv）
  * @param argc 命令行参数个数（来自main函数）
  * @param argv 命令行参数数组（来自main函数）
- * @param prog_name 程序名称（argv[0]，用于打印帮助/错误信息）
  * @return void
  */
-void args_parser(int argc, char* argv[]){
-    return;
+void args_parser(int argc, char* argv[]) {
+    // 程序名称
+    const char* prog_name = argv[0];
+
+    // 无参数：默认启动REPL
+    if (argc == 1) {
+        Repl repl;
+        return;
+    }
+
+    // 1个参数 : 处理 version/repl/help/路径
+    if (argc == 2) {
+        std::string cmd = argv[1];
+        if (cmd == "version") {
+            // 显示版本
+            printf("kiz version %s\n", KIZ_VERSION);
+        } else if (cmd == "repl") {
+            // 显式启动REPL
+            Repl repl;
+        } else if (cmd == "help") {
+            // 显示帮助信息
+            show_help(prog_name);
+        } else {
+            // ToDo: ...
+        }
+        return;
+    }
+
+    // 2个参数 : 仅处理 run <path>
+    if (argc == 3) {
+        std::string cmd = argv[1];
+        if (cmd == "run") {
+            std::string path = argv[2];
+            // ToDo: ...
+        } else {
+            // 无效命令
+            std::cerr << "错误: 无效指令 " << cmd << "\n";
+            show_help(prog_name);
+        }
+        return;
+    }
+
+    // 参数过多 : 提示错误并显示帮助
+    std::cerr << "错误: 太多参数";
+    show_help(prog_name);
 }
