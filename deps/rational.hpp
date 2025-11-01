0cbcc33ee7ee976dac09d9cf42916ab3ee716a3b
azhz1107cat <105213176+azhz1107cat@users.noreply.github.com>
2025-11-01 15:25:16 +0800

    更新 rational.hpp

--- deps/rational.hpp
+++ deps/rational.hpp
@@ -1,7 +1,15 @@
 /**
  * @file rational.hpp
- * @brief 有理数（Rational）核心定义
+ * @brief 有理数（Rational）核心定义与实现
  * 
  * @author azhz1107cat
  * @date 2025-10-25
  */
+
+namespace deps {
+
+class Rational {
+
+};
+
+} // namespace deps
