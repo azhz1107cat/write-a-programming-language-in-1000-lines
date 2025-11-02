48f5ab800e762056008ec7db143f400f912a770e
azhz1107cat <105213176+azhz1107cat@users.noreply.github.com>
2025-11-02 10:20:19 +0800

    更新 parse_expr.cpp

--- src/parse_expr.cpp
+++ src/parse_expr.cpp
@@ -6,20 +6,22 @@
  * @date 2025-10-25
  */
 
-std::unique_ptr<Expression> kiz::Parser::parse_expression() {
+namespace kiz {
+
+std::unique_ptr<Expression> Parser::parse_expression() {
     // ToDo: add op support 'and' 'or' 'not in' 'in'
     return parse_comparison();
 }
 
-std::unique_ptr<Expression> kiz::Parser::parse_comparison() {
+std::unique_ptr<Expression> Parser::parse_comparison() {
     auto node = parse_add_sub();
     while (
-        curr_token().type == kiz::TokenType::Equal
-        or curr_token().type == kiz::TokenType::NotEqual
-        or curr_token().type == kiz::TokenType::Greater
-        or curr_token().type == kiz::TokenType::Less
-        or curr_token().type == kiz::TokenType::GreaterEqual
-        or curr_token().type == kiz::TokenType::LessEqual
+        curr_token().type == TokenType::Equal
+        or curr_token().type == TokenType::NotEqual
+        or curr_token().type == TokenType::Greater
+        or curr_token().type == TokenType::Less
+        or curr_token().type == TokenType::GreaterEqual
+        or curr_token().type == TokenType::LessEqual
     ) {
         auto tok = curr_token();
         auto op = skip_token().text;
@@ -29,11 +31,11 @@ std::unique_ptr<Expression> kiz::Parser::parse_comparison() {
     return node;
 }
 
-std::unique_ptr<Expression> kiz::Parser::parse_add_sub() {
+std::unique_ptr<Expression> Parser::parse_add_sub() {
     auto node = parse_mul_div_mod();
     while (
-        curr_token().type == kiz::TokenType::Plus
-        or curr_token().type == kiz::TokenType::Minus
+        curr_token().type == TokenType::Plus
+        or curr_token().type == TokenType::Minus
     ) {
         auto tok = curr_token();
         auto op = skip_token().text;
@@ -43,12 +45,12 @@ std::unique_ptr<Expression> kiz::Parser::parse_add_sub() {
     return node;
 }
 
-std::unique_ptr<Expression> kiz::Parser::parse_mul_div_mod() {
+std::unique_ptr<Expression> Parser::parse_mul_div_mod() {
     auto node = parse_power();
     while (
-        curr_token().type == kiz::TokenType::Star
-        or curr_token().type == kiz::TokenType::Slash
-        or curr_token().type == kiz::TokenType::Percent
+        curr_token().type == TokenType::Star
+        or curr_token().type == TokenType::Slash
+        or curr_token().type == TokenType::Percent
     ) {
         auto tok = curr_token();
         auto op = skip_token().text;
@@ -58,9 +60,9 @@ std::unique_ptr<Expression> kiz::Parser::parse_mul_div_mod() {
     return node;
 }
 
-std::unique_ptr<Expression> kiz::Parser::parse_power() {
+std::unique_ptr<Expression> Parser::parse_power() {
     auto node = parse_unary();
-    if (curr_token().type == kiz::TokenType::Caret) {
+    if (curr_token().type == TokenType::Caret) {
         auto tok = curr_token();
         auto op = skip_token().text;
         auto right = parse_power();  // 右结合
@@ -69,9 +71,9 @@ std::unique_ptr<Expression> kiz::Parser::parse_power() {
     return node;
 }
 
-std::unique_ptr<Expression> kiz::Parser::parse_unary() {
+std::unique_ptr<Expression> Parser::parse_unary() {
     // ToDo: add op support 'not'
-    if (curr_token().type == kiz::TokenType::Minus) {
+    if (curr_token().type == TokenType::Minus) {
         auto tok = curr_token();
         skip_token();
         auto operand = parse_unary();
@@ -80,25 +82,25 @@ std::unique_ptr<Expression> kiz::Parser::parse_unary() {
     return parse_factor();
 }
 
-std::unique_ptr<Expression> kiz::Parser::parse_factor() {
+std::unique_ptr<Expression> Parser::parse_factor() {
     auto node = parse_primary();
 
     while (true) {
-        if (curr_token().type == kiz::TokenType::Dot) {
+        if (curr_token().type == TokenType::Dot) {
             skip_token(".");
             auto child = std::make_unique<IdentifierExpr>(skip_token().text);
             node = std::make_unique<GetMemberExpr>(std::move(node),std::move(child));
 
         }
-        else if (curr_token().type == kiz::TokenType::LBracket) {
+        else if (curr_token().type == TokenType::LBracket) {
             skip_token("[");
-            auto param = parse_params(kiz::TokenType::RBracket);
+            auto param = parse_params(TokenType::RBracket);
             skip_token("]");
             node = std::make_unique<GetItemExpr>(std::move(node),std::move(param));
         }
-        else if (curr_token().type == kiz::TokenType::LParen) {
+        else if (curr_token().type == TokenType::LParen) {
             skip_token("(");
-            auto param = parse_params(kiz::TokenType::RParen);
+            auto param = parse_params(TokenType::RParen);
             skip_token(")");
             node = std::make_unique<CallExpr>(std::move(node),std::move(param));
         }
@@ -107,33 +109,33 @@ std::unique_ptr<Expression> kiz::Parser::parse_factor() {
     return node;
 }
 
-std::unique_ptr<Expression> kiz::Parser::parse_primary() {
+std::unique_ptr<Expression> Parser::parse_primary() {
     const auto tok = skip_token();
-    if (tok.type == kiz::TokenType::Number) {
+    if (tok.type == TokenType::Number) {
         return std::make_unique<LiteralExpr>(tok.text);
     }
-    if (tok.type == kiz::TokenType::String) {
+    if (tok.type == TokenType::String) {
         return std::make_unique<LiteralExpr>(tok.text);
     }
-    if (tok.type == kiz::TokenType::Null) {
+    if (tok.type == TokenType::Null) {
         return std::make_unique<LiteralExpr>(tok.text, Value::Type::Null);
     }
-    if (tok.type == kiz::TokenType::True) {
+    if (tok.type == TokenType::True) {
         return std::make_unique<LiteralExpr>(tok.text, Value::Type::Bool);
     }
-    if (tok.type == kiz::TokenType::False) {
+    if (tok.type == TokenType::False) {
         return std::make_unique<LiteralExpr>(tok.text, Value::Type::Bool);
     }
-    if (tok.type == kiz::TokenType::Identifier) {
+    if (tok.type == TokenType::Identifier) {
         return std::make_unique<IdentifierExpr>(tok.text);
     }
-    if (tok.type == kiz::TokenType::Lambda) {
+    if (tok.type == TokenType::Lambda) {
         std::vector<std::string> params{};
-        if (curr_token().type == kiz::TokenType::Pipe) {
+        if (curr_token().type == TokenType::Pipe) {
             skip_token("|");
-            while (curr_token().type != kiz::TokenType::Pipe) {
+            while (curr_token().type != TokenType::Pipe) {
                 params.emplace_back(skip_token().text);
-                if (curr_token().type == kiz::TokenType::Comma) skip_token(",");
+                if (curr_token().type == TokenType::Comma) skip_token(",");
             }
             skip_token("|");
         }
@@ -143,11 +145,11 @@ std::unique_ptr<Expression> kiz::Parser::parse_primary() {
         skip_token("}");
         return std::make_unique<LambdaDeclExpr>("<lambda>", std::move(params),std::move(stmt));
     }
-    if (tok.type == kiz::TokenType::Pipe) {
+    if (tok.type == TokenType::Pipe) {
         std::vector<std::string> params;
-        while (curr_token().type != kiz::TokenType::Pipe) {
+        while (curr_token().type != TokenType::Pipe) {
             params.emplace_back(skip_token().text);
-            if (curr_token().type == kiz::TokenType::Comma) skip_token(",");
+            if (curr_token().type == TokenType::Comma) skip_token(",");
         }
         skip_token("|");
         auto expr = parse_expression();
@@ -160,25 +162,25 @@ std::unique_ptr<Expression> kiz::Parser::parse_primary() {
             std::make_unique<BlockStmt>(std::move(stmts))
         );
     }
-    if (tok.type == kiz::TokenType::LBrace) {
+    if (tok.type == TokenType::LBrace) {
         std::vector<std::pair<std::string, std::unique_ptr<Expression>>> init_vec{};
-        while (curr_token().type != kiz::TokenType::RBrace) {
+        while (curr_token().type != TokenType::RBrace) {
             auto key = skip_token().text;
             skip_token("=");
             auto val = parse_expression();
-            if (curr_token().type == kiz::TokenType::Comma) skip_token(",");
-            if (curr_token().type == kiz::TokenType::Semicolon) skip_token(";");
+            if (curr_token().type == TokenType::Comma) skip_token(",");
+            if (curr_token().type == TokenType::Semicolon) skip_token(";");
             init_vec.emplace_back(std::move(key), std::move(val));
         }
         skip_token("}");
         return std::make_unique<LambdaStructDeclExpr>(std::move(init_vec));
     }
-    if (tok.type == kiz::TokenType::LBracket) {
-        auto param = parse_params(kiz::TokenType::RBracket);
+    if (tok.type == TokenType::LBracket) {
+        auto param = parse_params(TokenType::RBracket);
         skip_token("]");
         return std::make_unique<ArrayExpr>(std::move(param));
     }
-    if (tok.type == kiz::TokenType::LParen) {
+    if (tok.type == TokenType::LParen) {
         auto expr = parse_expression();
         skip_token(")");
         return expr;
@@ -186,11 +188,13 @@ std::unique_ptr<Expression> kiz::Parser::parse_primary() {
     return nullptr;
 }
 
-std::vector<std::unique_ptr<Expression>> kiz::Parser::parse_params(const kiz::TokenType endswith){
+std::vector<std::unique_ptr<Expression>> Parser::parse_params(const TokenType endswith){
     std::vector<std::unique_ptr<Expression>> params;
     while (curr_token().type != endswith) {
         params.emplace_back(parse_expression());
-        if (curr_token().type == kiz::TokenType::Comma) skip_token(",");
+        if (curr_token().type == TokenType::Comma) skip_token(",");
     }
     return params;
 }
+
+} // namespace kiz
