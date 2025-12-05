8c3de6cc3ab63772bef09f1b711051a1b7109386
azhz1107cat <105213176+azhz1107cat@users.noreply.github.com>
2025-12-05 18:00:28 +0800

    更新 registering_std_modules.cpp

--- src/vm/registering_std_modules.cpp
+++ src/vm/registering_std_modules.cpp
@@ -3,7 +3,9 @@
 namespace model {
 
 void registering_std_modules() {
-    std_modules.insert("math", nullptr);
+    std_modules.insert("math", new CppFunction(
+        math_lib::__init_module__
+    ));
 }
 
 } // namespace model
